0bc75ad891337280372b3eedea0c49d416deb000
Angel Canul <angelantoniocanulchin07@gmail.com>
2024-05-28 15:33:56 -0600

    Creacion_expedientes_Usuarios_admin

--- resources/views/expedientes/index.blade.php
+++ resources/views/expedientes/index.blade.php
@@ -3,11 +3,34 @@
 @section('title', 'Expediente')
 
 @section('content_header')
-    <h1>Expedientes Usuarios</h1>
+    <h1>USUARIOS</h1>
 @stop
 
 @section('content')
-    <p>Gestion de Usuarios.</p>
+    <p>EXPEDIENTES</p>
+    <table style="width: 100%; border-collapse: collapse; border: 1px solid #ddd;">
+        <thead>
+            <tr style="background-color: #f2f2f2;">
+                <th style="padding: 8px; text-align: left;">id</th>
+                <th style="padding: 8px; text-align: left;">Nombre</th>
+                <th style="padding: 8px; text-align: left;">Apellido</th>
+                <th style="padding: 8px; text-align: left;">Nombre</th>
+                <th style="padding: 8px; text-align: left;">Nombre</th>
+                <th style="padding: 8px; text-align: left;">Nombre</th>
+                <th style="padding: 8px; text-align: left;">Nombre</th>
+
+            </tr>
+        </thead>
+        <tbody>
+            <tr style="background-color: #f9f9f9;">
+                <td style="padding: 8px; text-align: left;">Dato 1</td>
+                <td style="padding: 8px; text-align: left;">Dato 2</td>
+                <!-- Agrega más datos según sea necesario -->
+            </tr>
+            <!-- Agrega más filas de datos según sea necesario -->
+        </tbody>
+    </table>
+
 
 @stop
 
