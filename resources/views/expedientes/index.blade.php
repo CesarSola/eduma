000589a608ab5176e7df8268f634a31aa0484252
Angel Canul <angelantoniocanulchin07@gmail.com>
2024-05-28 15:43:28 -0600

    tabla

--- resources/views/expedientes/index.blade.php
+++ resources/views/expedientes/index.blade.php
@@ -8,30 +8,37 @@
 
 @section('content')
     <p>EXPEDIENTES</p>
-    <table style="width: 100%; border-collapse: collapse; border: 1px solid #ddd;">
+
+    <table class="table table-striped table-hover">
         <thead>
             <tr style="background-color: #f2f2f2;">
-                <th style="padding: 8px; text-align: left;">id</th>
-                <th style="padding: 8px; text-align: left;">Nombre</th>
-                <th style="padding: 8px; text-align: left;">Apellido</th>
-                <th style="padding: 8px; text-align: left;">Nombre</th>
-                <th style="padding: 8px; text-align: left;">Nombre</th>
-                <th style="padding: 8px; text-align: left;">Nombre</th>
-                <th style="padding: 8px; text-align: left;">Nombre</th>
-
+                <th style="padding: 8px; text-align: center; border-right: 2px solid color: black;">id</th>
+                <th style="padding: 8px; text-align: center; border-right: 2px solid color: black;">Nombre</th>
+                <th style="padding: 8px; text-align: center; border-right: 2px solid color: black;">Apellido</th>
+                <th style="padding: 8px; text-align: center; border-right: 2px solid color: black;">Matrícula</th>
+                <th style="padding: 8px; text-align: center; border-right: 2px solid color: black;">INE</th>
+                <th style="padding: 8px; text-align: center; border-right: 2px solid color: black;">Comprobante Domiciliario
+                </th>
+                <th style="padding: 8px; text-align: center; border-right: 2px solid color: black;">Foto</th>
+                <th style="padding: 8px; text-align: center; border-right: 2px solid color: black;">Estado</th>
+                <th style="padding: 8px; text-align: center; border-right: 2px solid color: black;">Expediente</th>
             </tr>
         </thead>
         <tbody>
             <tr style="background-color: #f9f9f9;">
-                <td style="padding: 8px; text-align: left;">Dato 1</td>
-                <td style="padding: 8px; text-align: left;">Dato 2</td>
-                <!-- Agrega más datos según sea necesario -->
+                <td style="padding: 8px; text-align: center; border-right: 2px solid color: black;">Dato 1</td>
+                <td style="padding: 8px; text-align: center; border-right: 2px solid color: black;">Dato 2</td>
+                <td style="padding: 8px; text-align: center; border-right: 2px solid color: black;">Dato 1</td>
+                <td style="padding: 8px; text-align: center; border-right: 2px solid color: black;">Dato 2</td>
+                <td style="padding: 8px; text-align: center; border-right: 2px solid color: black;">Dato 1</td>
+                <td style="padding: 8px; text-align: center; border-right: 2px solid color: black;">Dato 2</td>
+                <td style="padding: 8px; text-align: center; border-right: 2px solid color: black;">Dato 1</td>
+                <td style="padding: 8px; text-align: center; border-right: 2px solid color: black;">Dato 2</td>
+                <td style="padding: 8px; text-align: center; border-right: 2px solid color: black;">Dato 2</td>
             </tr>
             <!-- Agrega más filas de datos según sea necesario -->
         </tbody>
     </table>
-
-
 @stop
 
 @section('css')
