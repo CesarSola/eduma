ccf4d09db90277cd154b8c81526132e5c0c7827d
Angel Canul <angelantoniocanulchin07@gmail.com>
2024-05-29 11:07:33 -0600

    vista expediente

--- resources/views/expedientes/usuarios/index.blade.php
+++ resources/views/expedientes/usuarios/index.blade.php
@@ -3,35 +3,74 @@
 @section('title', 'Expediente')
 
 @section('content_header')
-    <div class="header-flex">
-        <h1>EXPEDIENTE</h1>
-        <div>
-            <form action="{{ route('expedientes.index') }}" method="GET">
-                <button type="submit" class="btn btn-primary">regresar</button>
-            </form>
-        </div>
-    </div>
+    <h1>Expediente</h1>
 @stop
 
 @section('content')
-    <div class="content-flex">
-        <div class="left-content">
-            <p>Documentos de Registro General</p>
-            <form action="{{ route('registroGeneral.index') }}" method="GET">
-                <button type="submit" class="btn btn-primary">Ver Documentos</button>
-            </form>
-        </div>
-        <div class="right-content">
-            <p>Competencias</p>
-            <form action="{{ route('competencias.index') }}" method="GET">
-                <button type="submit" class="btn btn-primary">Ver Competencias</button>
-            </form>
-            <p>Cursos</p>
-            <form action="{{ route('cursos.index') }}" method="GET">
-                <button type="submit" class="btn btn-primary">Ver Cursos</button>
-            </form>
+    <div class="row">
+        <div class="col-md-3">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Datos Personales</h3>
+                </div>
+                <div class="card-body">
+                    <div class="text-center">
+                        <img src="{{ asset('path_to_default_avatar') }}" alt="User Avatar" class="img-circle">
+                    </div>
+                    <h4 class="text-center mt-2">Nombre | Apellido</h4>
+                    <p class="text-muted text-center">Datos Personales</p>
+                    <a href="#" class="btn btn-success btn-block">Editar</a>
+                </div>
+            </div>
         </div>
 
+        <div class="col-md-9">
+            <div class="card">
+                <div class="card-header">
+                    <h3 class="card-title">Expediente</h3>
+                    <div class="card-tools">
+                        <span class="badge badge-info">Estatus: Activo</span>
+                    </div>
+                </div>
+                <div class="card-body">
+                    <div class="row">
+                        <div class="col-md-6">
+                            <h4>Documentos de Registro General</h4>
+                            <ul class="list-group">
+                                <li class="list-group-item">Documento 1</li>
+                                <li class="list-group-item">Documento 2</li>
+                                <li class="list-group-item">Documento 3</li>
+                                <li class="list-group-item">Documento 4</li>
+                            </ul>
+                            <a href="{{ route('registroGeneral.index') }}" class="btn btn-primary btn-block mt-2">Ver</a>
+                        </div>
+                        <div class="col-md-6">
+                            <h4>Competencias | Cursos</h4>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <ul class="list-group">
+                                        <li class="list-group-item">Competencia 1</li>
+                                        <li class="list-group-item">Competencia 2</li>
+                                        <li class="list-group-item">Competencia 3</li>
+                                    </ul>
+                                    <a href="{{ route('cursos.index') }}" class="btn btn-primary btn-block mt-2">Ver</a>
+                                </div>
+                                <div class="col-md-6">
+                                    <ul class="list-group">
+                                        <li class="list-group-item">Curso 1</li>
+                                        <li class="list-group-item">Curso 2</li>
+                                        <li class="list-group-item">Curso 3</li>
+                                    </ul>
+                                    <a href="{{ route('competencias.index') }}"
+                                        class="btn btn-primary btn-block mt-2">Ver</a>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @stop
 
