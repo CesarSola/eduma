918a2df75f526258b836b76544ab39e558624beb
Angel Canul <angelantoniocanulchin07@gmail.com>
2024-05-29 11:16:29 -0600

    vista_expediente

--- resources/views/expedientes/usuarios/index.blade.php
+++ resources/views/expedientes/usuarios/index.blade.php
@@ -8,34 +8,34 @@
 
 @section('content')
     <div class="row">
-        <div class="col-md-3">
+        <div class="col-md-12">
             <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">Datos Personales</h3>
-                </div>
-                <div class="card-body">
-                    <div class="text-center">
-                        <img src="{{ asset('path_to_default_avatar') }}" alt="User Avatar" class="img-circle">
+                <div class="card-body header-flex">
+                    <div class="left-content">
+                        <div class="text-center">
+                            <img src="{{ asset('path_to_default_avatar') }}" alt="" class="img-circle">
+                        </div>
+                        <h6 class="text-left mt-2">Nombre</h6>
+                        <h6 class="text-left mt-2">Apellido</h6>
+                        <h6 class="text-left mt-2">Edad: 30 años</h6>
+
+                        <a href="#" class="btn btn-success btn-block">Editar</a>
                     </div>
-                    <h4 class="text-center mt-2">Nombre | Apellido</h4>
-                    <p class="text-muted text-center">Datos Personales</p>
-                    <a href="#" class="btn btn-success btn-block">Editar</a>
+                    <div class="right-content">
+                        <span class="badge badge-info">Estatus: Activo</span>
+                    </div>
                 </div>
             </div>
         </div>
 
-        <div class="col-md-9">
-            <div class="card">
-                <div class="card-header">
-                    <h3 class="card-title">Expediente</h3>
-                    <div class="card-tools">
-                        <span class="badge badge-info">Estatus: Activo</span>
-                    </div>
-                </div>
-                <div class="card-body">
-                    <div class="row">
-                        <div class="col-md-6">
-                            <h4>Documentos de Registro General</h4>
+        <div class="col-md-12 mt-3">
+            <div class="row">
+                <div class="col-md-6">
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">Documentos de Registro General</h3>
+                        </div>
+                        <div class="card-body">
                             <ul class="list-group">
                                 <li class="list-group-item">Documento 1</li>
                                 <li class="list-group-item">Documento 2</li>
@@ -44,28 +44,40 @@
                             </ul>
                             <a href="{{ route('registroGeneral.index') }}" class="btn btn-primary btn-block mt-2">Ver</a>
                         </div>
+                    </div>
+                </div>
+                <div class="col-md-6">
+                    <div class="row">
                         <div class="col-md-6">
-                            <h4>Competencias | Cursos</h4>
-                            <div class="row">
-                                <div class="col-md-6">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h3 class="card-title">Competencias</h3>
+                                </div>
+                                <div class="card-body">
                                     <ul class="list-group">
                                         <li class="list-group-item">Competencia 1</li>
                                         <li class="list-group-item">Competencia 2</li>
                                         <li class="list-group-item">Competencia 3</li>
                                     </ul>
-                                    <a href="{{ route('cursos.index') }}" class="btn btn-primary btn-block mt-2">Ver</a>
+                                    <a href="{{ route('competencias.index') }}"
+                                        class="btn btn-primary btn-block mt-2">Ver</a>
                                 </div>
-                                <div class="col-md-6">
+                            </div>
+                        </div>
+                        <div class="col-md-6">
+                            <div class="card">
+                                <div class="card-header">
+                                    <h3 class="card-title">Cursos</h3>
+                                </div>
+                                <div class="card-body">
                                     <ul class="list-group">
                                         <li class="list-group-item">Curso 1</li>
                                         <li class="list-group-item">Curso 2</li>
                                         <li class="list-group-item">Curso 3</li>
                                     </ul>
-                                    <a href="{{ route('competencias.index') }}"
-                                        class="btn btn-primary btn-block mt-2">Ver</a>
+                                    <a href="{{ route('cursos.index') }}" class="btn btn-primary btn-block mt-2">Ver</a>
                                 </div>
                             </div>
-
                         </div>
                     </div>
                 </div>
@@ -82,25 +94,13 @@
             align-items: center;
         }
 
-        .content-flex {
-            display: flex;
-            justify-content: space-between;
-            align-items: flex-start;
-        }
-
         .left-content {
-            width: 50%;
-            float: left;
+            width: 70%;
         }
 
         .right-content {
-            width: 50%;
-            float: right;
+            width: 30%;
             text-align: right;
-        }
-
-        .button-right {
-            float: right;
         }
     </style>
 @stop
