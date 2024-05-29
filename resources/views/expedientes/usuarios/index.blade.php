0afa6507fad457d7badc5349bc92fd6fd061bed8
Angel Canul <angelantoniocanulchin07@gmail.com>
2024-05-29 11:55:50 -0600

    vista_terminada

--- resources/views/expedientes/usuarios/index.blade.php
+++ resources/views/expedientes/usuarios/index.blade.php
@@ -3,7 +3,10 @@
 @section('title', 'Expediente')
 
 @section('content_header')
-    <h1>Expediente</h1>
+    <div class="d-flex justify-content-between align-items-center">
+        <h1>Expediente</h1>
+        <a href="{{ route('expedientes.index') }}" class="btn btn-secondary">Regresar</a>
+    </div>
 @stop
 
 @section('content')
@@ -18,8 +21,7 @@
                         <h6 class="text-left mt-2">Nombre</h6>
                         <h6 class="text-left mt-2">Apellido</h6>
                         <h6 class="text-left mt-2">Edad: 30 años</h6>
-
-                        <a href="#" class="btn btn-success btn-block">Editar</a>
+                        <a href="#" class="btn btn-success btn-block btn-sm">Editar</a>
                     </div>
                     <div class="right-content">
                         <span class="badge badge-info">Estatus: Activo</span>
@@ -31,51 +33,66 @@
         <div class="col-md-12 mt-3">
             <div class="row">
                 <div class="col-md-6">
-                    <div class="card">
+                    <div class="card h-100">
                         <div class="card-header">
                             <h3 class="card-title">Documentos de Registro General</h3>
                         </div>
-                        <div class="card-body">
-                            <ul class="list-group">
+                        <div class="card-body d-flex flex-column">
+                            <ul class="list-group flex-grow-1 overflow-auto">
                                 <li class="list-group-item">Documento 1</li>
                                 <li class="list-group-item">Documento 2</li>
                                 <li class="list-group-item">Documento 3</li>
                                 <li class="list-group-item">Documento 4</li>
+                                <li class="list-group-item">Documento 5</li>
+                                <li class="list-group-item">Documento 6</li>
+                                <li class="list-group-item">Documento 7</li>
+                                <li class="list-group-item">Documento 8</li>
+                                <li class="list-group-item">Documento 9</li>
                             </ul>
-                            <a href="{{ route('registroGeneral.index') }}" class="btn btn-primary btn-block mt-2">Ver</a>
+                            <a href="{{ route('registroGeneral.index') }}"
+                                class="btn btn-primary btn-block btn-sm mt-2">Ver</a>
                         </div>
                     </div>
                 </div>
                 <div class="col-md-6">
-                    <div class="row">
+                    <div class="row h-100">
                         <div class="col-md-6">
-                            <div class="card">
+                            <div class="card h-100">
                                 <div class="card-header">
                                     <h3 class="card-title">Competencias</h3>
                                 </div>
-                                <div class="card-body">
-                                    <ul class="list-group">
+                                <div class="card-body d-flex flex-column">
+                                    <ul class="list-group flex-grow-1 overflow-auto">
                                         <li class="list-group-item">Competencia 1</li>
                                         <li class="list-group-item">Competencia 2</li>
                                         <li class="list-group-item">Competencia 3</li>
+                                        <li class="list-group-item">Competencia 4</li>
+                                        <li class="list-group-item">Competencia 5</li>
+                                        <li class="list-group-item">Competencia 6</li>
+                                        <li class="list-group-item">Competencia 7</li>
                                     </ul>
                                     <a href="{{ route('competencias.index') }}"
-                                        class="btn btn-primary btn-block mt-2">Ver</a>
+                                        class="btn btn-primary btn-block btn-sm mt-2">Ver</a>
                                 </div>
                             </div>
                         </div>
                         <div class="col-md-6">
-                            <div class="card">
+                            <div class="card h-100">
                                 <div class="card-header">
                                     <h3 class="card-title">Cursos</h3>
                                 </div>
-                                <div class="card-body">
-                                    <ul class="list-group">
+                                <div class="card-body d-flex flex-column">
+                                    <ul class="list-group flex-grow-1 overflow-auto">
                                         <li class="list-group-item">Curso 1</li>
                                         <li class="list-group-item">Curso 2</li>
                                         <li class="list-group-item">Curso 3</li>
+                                        <li class="list-group-item">Curso 4</li>
+                                        <li class="list-group-item">Curso 5</li>
+                                        <li class="list-group-item">Curso 6</li>
+                                        <li class="list-group-item">Curso 7</li>
                                     </ul>
-                                    <a href="{{ route('cursos.index') }}" class="btn btn-primary btn-block mt-2">Ver</a>
+                                    <a href="{{ route('cursos.index') }}"
+                                        class="btn btn-primary btn-block btn-sm mt-2">Ver</a>
                                 </div>
                             </div>
                         </div>
@@ -101,6 +118,43 @@
         .right-content {
             width: 30%;
             text-align: right;
+        }
+
+        .card-title {
+            text-align: center;
+            width: 100%;
+        }
+
+        .list-group-item {
+            text-align: center;
+            width: 100%;
+        }
+
+        .h-100 {
+            height: 100%;
+        }
+
+        .overflow-auto {
+            max-height: 200px;
+            /* Ajusta esta altura según sea necesario */
+            overflow-y: auto;
+        }
+
+        .btn-secondary {
+            margin-left: auto;
+        }
+
+        .btn-success {
+            align-content: center;
+            width: 50%;
+        }
+
+        .btn-primary {
+            width: 100%;
+        }
+
+        .btn-sm {
+            padding: 0.25rem 0.5rem;
         }
     </style>
 @stop
