61e4ef7c3e0c4edf946d85de4924f2ab4ae90bed
Angel Canul <angelantoniocanulchin07@gmail.com>
2024-05-31 12:47:23 -0600

    dash_usuario

--- resources/views/expedientes/expedientesUser/dashboardUser/index.blade.php
+++ resources/views/expedientes/expedientesUser/dashboardUser/index.blade.php
@@ -3,49 +3,114 @@
 @section('title', 'SICE')
 
 @section('content_header')
-    <div class="row">
-        <div class="col-md-12">
-            <div class="card">
-                <div class="card-body header-flex">
-                    <div class="left-content">
-                        <div class="text-center">
-                            <p>SICE</p>
-                        </div>
-                    </div>
+
+    <div class="card">
+        <div class="card-body">
+            <div class="left-content">
+                <div class="text-center">
+                    <p>SICE</p>
                 </div>
             </div>
-        @stop
+        </div>
+    </div>
+@stop
 
-        @section('content')
-            <div class="row">
-                <div class="col-md-12">
+@section('content')
+    <div class="card">
+        <div class="card-body">
+            <div class="card-header">
+                <h4>Bienvenido</h4>
+                <div class="card-title">
+                    <h6 class="text">NOMBRE DEL PARTICIPANTE</h6>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <br>
+    <div class="card">
+        <h6 style="text-align: center" class="card-title">Completa los siguientes pasos</h6>
+        <br>
+        <div class="card">
+            <div class="card-body">
+                <h6 class="text-center">Lista de requerimentos y documentación</h6>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <h6 class="text-center">Sube tus documentos</h6>
+            </div>
+        </div>
+        <div class="card">
+            <div class="card-body">
+                <h6 class="text-center">Descargar los formatos</h6>
+            </div>
+        </div>
+    </div>
+    <div class="card">
+        <h6 style="text-align: center" class="card-title">Registrate a la evaluación de un EC</h6>
+        <div class="card-body">
+            <div class="card">
+                <div class="card-body">
                     <div class="card">
-                        <div class="card-title flex">
-                            <div class="left-content">
-                                <div class="card-header">
-                                    <p>Bienvenidos</p>
-                                </div>
-                                <div class="card-body">
-                                    <h5 class="card">NOMBRE DEL PARTICIPANTE</h5>
-                                </div>
-                                <div class="card-body">
-                                    Completa los siguientes pasos
-                                </div>
-                            </div>
+                        <div class="card-body">
+                            <h6 class="text-left">EC01</h6>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <h6 class="text-left">EC12</h6>
+                        </div>
+                    </div>
+                    <div class="card">
+                        <div class="card-body">
+                            <h6 class="text-left">EC05</h6>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    @stop
+    </div>
+    </div>
+    </div>
+    </div>
 
-    @section('css')
-        {{-- Add here extra stylesheets --}}
-        {{-- <link rel="stylesheet" href="/css/admin_custom.css"> --}}
-    @stop
+@stop
 
-    @section('js')
-        <script>
-            console.log("Hi, I'm using the Laravel-AdminLTE package!");
-        </script>
-    @stop
+@section('css')
+    <style>
+        .card-title {
+            background-color: #5cb85c;
+            /* Color verde */
+            padding: 10px;
+            color: white;
+            border-radius: 5px;
+        }
+
+        .card-header h3 {
+            margin: 0;
+        }
+
+        .card-body {
+            background-color: #dff0d8;
+            /* Fondo verde claro */
+            padding: 20px;
+            border: 1px solid #5cb85c;
+            border-radius: 5px;
+        }
+
+        .text-center {
+            color: #000;
+        }
+
+        .text-left {
+            color: #000
+        }
+    </style>
+@stop
+
+@section('js')
+    <script>
+        console.log("Hi, I'm using the Laravel-AdminLTE package!");
+    </script>
+@stop
