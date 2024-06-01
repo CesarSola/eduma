6f8bbc26ce25330258e3abd3d9163c04af19d71c
Angel Canul <angelantoniocanulchin07@gmail.com>
2024-05-31 21:16:12 -0600

    dash

--- resources/views/expedientes/expedientesUser/registroEC/index.blade.php
+++ resources/views/expedientes/expedientesUser/registroEC/index.blade.php
@@ -4,37 +4,49 @@
 
 @section('content_header')
     <div class="card">
-        <div class="card-body">
-            <div class="left-content">
-                <div class="text-center">
-                    <p>SICE</p>
-                </div>
-            </div>
+        <div class="card-body-1 text-center-1">
+            <p>REGISTRO A UN EC</p>
         </div>
     </div>
 @stop
 
 @section('content')
+    <div class="card mb-3" style="max-width: 600px; margin: auto;">
+        <div class="card-body-1 d-flex justify-content-between">
+            <div class="d-flex flex-column">
+                <h6 class="text-left">EC001</h6>
+            </div>
+            <div class="d-flex flex-column flex-grow-1 text-center">
+                <h6 class="text-center">NOMBRE DEL ESTÁNDAR DE C</h6>
+            </div>
+        </div>
+    </div>
 
+    <div class="d-flex flex-column mb-3">
+        <div class="card mb-3" style="width: 48%; align-self: flex-start;">
+            <div class="card-body">
+                <h6>REQUISITOS PARA LA EVALUACIÓN Y CERTIFICACIÓN</h6>
+                <h6>INFORMACIÓN DEL CURSO</h6>
+                <h6>LISTA DE EVIDENCIAS</h6>
+            </div>
+        </div>
 
-    <div class="card">
-        <h6 style="text-align: center" class="card-title">Completa los siguientes pasos</h6>
-        <br>
-        <div class="card">
-            <div class="card-body">
-                <h6 class="text-center">Lista de requerimentos y documentación</h6>
+        <div class="d-flex justify-content-between" style="width: 100%;">
+            <div class="card mb-3" style="width: 48%;">
+                <div class="card-body text-center">
+                    <button class="btn btn-success">SUBIR DOCUMENTOS DE INSCRIPCIÓN</button>
+                </div>
+            </div>
+            <div class="card mb-3" style="width: 48%;">
+                <div class="card-body text-center">
+                    <button class="btn btn-success">SUBIR COMPROBANTE DE PAGO</button>
+                </div>
             </div>
         </div>
-        <div class="card">
-            <div class="card-body">
-                <h6 class="text-center">Sube tus documentos</h6>
-            </div>
-        </div>
-        <div class="card">
-            <div class="card-body">
-                <h6 class="text-center">Descargar los formatos</h6>
-            </div>
-        </div>
+    </div>
+
+    <div class="text-center mt-3">
+        <button class="btn btn-success">EVIDENCIAS</button>
     </div>
 @stop
 
@@ -42,7 +54,6 @@
     <style>
         .card-title {
             background-color: #5cb85c;
-            /* Color verde */
             padding: 10px;
             color: white;
             border-radius: 5px;
@@ -54,14 +65,27 @@
 
         .card-body {
             background-color: #dff0d8;
-            /* Fondo verde claro */
             padding: 20px;
             border: 1px solid #5cb85c;
             border-radius: 5px;
         }
 
+        .card-body-1 {
+            background-color: #5cb85c;
+            padding: 10px;
+            border: 1px solid #5cb85c;
+            border-radius: 5px;
+            color: #fff;
+            text-align: center;
+        }
+
         .text-center {
             color: #000;
+        }
+
+        .text-center-1 {
+            color: #fff;
+            text-align: center;
         }
 
         .text-left {
@@ -70,6 +94,17 @@
 
         .d-flex.align-items-center h6 {
             margin-bottom: 0;
+        }
+
+        .btn-success {
+            background-color: #5cb85c;
+            border-color: #5cb85c;
+            color: white;
+        }
+
+        .btn-success:hover {
+            background-color: #4cae4c;
+            border-color: #4cae4c;
         }
     </style>
 @stop
