3cdfbefe6f94697c4cca22279f42c0fc59f31113
Angel Canul <angelantoniocanulchin07@gmail.com>
2024-06-03 18:22:21 -0600

    descarga

--- config/app.php
+++ config/app.php
@@ -161,13 +161,12 @@ return [
         /*
          * Package Service Providers...
          */
-        Maatwebsite\Excel\ExcelServiceProvider::class,
-  
+
         /*
          * Application Service Providers...
          */
         App\Providers\AppServiceProvider::class,
- 
+
         App\Providers\AuthServiceProvider::class,
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
@@ -185,9 +184,6 @@ return [
     |
     */
 
-    'aliases' => Facade::defaultAliases()->merge([
-        // 'Example' => App\Facades\Example::class,
-        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
-    ])->toArray(),
+    'aliases' => Facade::defaultAliases()->merge([])->toArray(),
 
 ];
