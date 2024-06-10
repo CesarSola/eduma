768d6a12bd60cc4799fb9d18c43e109c12dd6895
Angel Canul <angelantoniocanulchin07@gmail.com>
2024-06-10 14:55:15 -0600

    CP

--- app/Imports/CodigosPostalesImport.php
+++ app/Imports/CodigosPostalesImport.php
@@ -3,9 +3,11 @@
 namespace App\Imports;
 
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
 use App\Models\CodigoPostal;
+use Illuminate\Support\Facades\Log;
 
-class CodigosPostalesImport implements ToModel
+class CodigosPostalesImport implements ToModel, WithHeadingRow
 {
     /**
      * @param array $row
@@ -14,22 +16,34 @@ class CodigosPostalesImport implements ToModel
      */
     public function model(array $row)
     {
-        return new CodigoPostal([
-            'd_codigo' => $row[0], // Código Postal
-            'd_asenta' => $row[1], // Asentamiento
-            'd_tipo_asenta' => $row[2], // Tipo Asentamiento
-            'D_mnpio' => $row[3], // Delegación/Municipio
-            'd_estado' => $row[4], // Estado
-            'd_ciudad' => $row[5], // Ciudad
-            'd_CP' => $row[6], // Código Postal (segunda ocurrencia)
-            'c_estado' => $row[7], // Código Estado
-            'c_oficina' => $row[8], // Código Oficina
-            'c_tipo_asenta' => $row[9], // Código Tipo Asentamiento
-            'c_mnpio' => $row[10], // Código Municipio
-            'id_asenta_cpcons' => $row[11], // ID Asentamiento CP Consulta
-            'd_zona' => $row[12], // Zona
-            'c_cve_ciudad' => $row[13], // Código Clave Ciudad
-            // Agrega aquí más campos si es necesario
-        ]);
+        // Verificar que los campos críticos no sean nulos
+        if (
+            !is_null($row['d_codigo']) && !is_null($row['d_asenta']) && !is_null($row['d_tipo_asenta']) &&
+            !is_null($row['d_mnpio']) && !is_null($row['d_estado']) && !is_null($row['d_ciudad']) &&
+            !is_null($row['d_cp']) && !is_null($row['c_estado']) && !is_null($row['c_oficina']) &&
+            !is_null($row['c_tipo_asenta']) && !is_null($row['c_mnpio']) && !is_null($row['id_asenta_cpcons']) &&
+            !is_null($row['d_zona']) && !is_null($row['c_cve_ciudad'])
+        ) {
+            return new CodigoPostal([
+                'd_codigo' => $row['d_codigo'], // Código Postal
+                'd_asenta' => $row['d_asenta'], // Asentamiento
+                'd_tipo_asenta' => $row['d_tipo_asenta'], // Tipo Asentamiento
+                'D_mnpio' => $row['d_mnpio'], // Delegación/Municipio
+                'd_estado' => $row['d_estado'], // Estado
+                'd_ciudad' => $row['d_ciudad'], // Ciudad
+                'd_CP' => $row['d_cp'], // Código Postal (segunda ocurrencia)
+                'c_estado' => $row['c_estado'], // Código Estado
+                'c_oficina' => $row['c_oficina'], // Código Oficina
+                'c_tipo_asenta' => $row['c_tipo_asenta'], // Código Tipo Asentamiento
+                'c_mnpio' => $row['c_mnpio'], // Código Municipio
+                'id_asenta_cpcons' => $row['id_asenta_cpcons'], // ID Asentamiento CP Consulta
+                'd_zona' => $row['d_zona'], // Zona
+                'c_cve_ciudad' => $row['c_cve_ciudad'], // Código Clave Ciudad
+            ]);
+        } else {
+            // Registrar un log para las filas incompletas o con valores nulos en campos obligatorios
+            Log::warning('Fila incompleta o con valores nulos encontrada', ['row' => $row]);
+            return null;
+        }
     }
 }
--- resources/views/codigos-postales/index.blade.php
+++ resources/views/codigos-postales/index.blade.php
@@ -3,8 +3,9 @@
 @section('title', 'Codigos postales')
 
 @section('content_header')
-<script src="https://kit.fontawesome.com/646ac4fad6.js" crossorigin="anonymous"></script>
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css" integrity="sha384-xrR6LJoJdBJ5D4qz7f8K94JPPIU5xN9xnecb76WywRJ2OLqFz1EGgM5V1WQ1gXQQ" crossorigin="anonymous">
+    <script src="https://kit.fontawesome.com/646ac4fad6.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.1/css/all.min.css"
+        integrity="sha384-xrR6LJoJdBJ5D4qz7f8K94JPPIU5xN9xnecb76WywRJ2OLqFz1EGgM5V1WQ1gXQQ" crossorigin="anonymous">
 
     <h1>Codigos postales</h1>
 @stop
@@ -12,98 +13,98 @@
 @section('content')
 
 
-<div class="row">
-   @can('permissions.create') 
-    <div class="float-right">
-        <a href="{{ route('permissions.create') }}" class="btn btn-success btn-sm float-right"  data-placement="left">
-          {{ __('Agregar') }}
-        </a>
-      </div> 
-    @endcan  
-</div>
-<form action="{{ route('importar.excel') }}" method="POST" enctype="multipart/form-data">
-    @csrf
-    <div class="form-group">
-        <label for="archivo_excel">Seleccione un archivo Excel:</label>
-        <input type="file" class="form-control-file" id="archivo_excel" name="archivo_excel">
+    <div class="row">
+        @can('permissions.create')
+            <div class="float-right">
+                <a href="{{ route('permissions.create') }}" class="btn btn-success btn-sm float-right" data-placement="left">
+                    {{ __('Agregar') }}
+                </a>
+            </div>
+        @endcan
     </div>
-    <button type="submit" class="btn btn-primary">Importar</button>
-</form>
-<div class="card-body px-0 pb-2">
-    <table class="table table-bordered">
-        <thead class="table-success">
-            <tr>
-                <th>ID</th>
-                <th>Código Postal</th>
-                <th>Asentamiento</th>
-                <th>Tipo Asentamiento</th>
-                <th>Delegacion/Municipio</th>
-                <th>Estado</th>
-                <th>Cuidad</th>
-                <th>CODIGO POSTAL</th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-                <th></th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($codigosPostales as $codigoPostal)
-            <tr>
-                <td>{{ $codigoPostal->id }}</td>
-                <td>{{ $codigoPostal->d_codigo }}</td>
-                <td>{{ $codigoPostal->d_asenta }}</td>
-                <td>{{ $codigoPostal->d_tipo_asenta }}</td>
-                <td>{{ $codigoPostal->D_mnpio }}</td>
-                <td>{{ $codigoPostal->d_estado }}</td>
-                <td>{{ $codigoPostal->d_ciudad }}</td>
-                <td>{{ $codigoPostal->d_CP }}</td>
-                <td>{{ $codigoPostal->c_estado }}</td>
-                <td>{{ $codigoPostal->c_oficina }}</td>
-                <td>{{ $codigoPostal->c_tipo_asenta }}</td>
-                <td>{{ $codigoPostal->c_mnpio }}</td>
-                <td>{{ $codigoPostal->id_asenta_cpcons }}</td>
-                <td>{{ $codigoPostal->d_zona }}</td>
-                <td>{{ $codigoPostal->c_cve_ciudad }}</td>
-              
-            </tr>
-            @endforeach
+    <form action="{{ route('importar.excel') }}" method="POST" enctype="multipart/form-data">
+        @csrf
+        <div class="form-group">
+            <label for="archivo_excel">Seleccione un archivo Excel:</label>
+            <input type="file" class="form-control-file" id="archivo_excel" name="archivo_excel">
+        </div>
+        <button type="submit" class="btn btn-primary">Importar</button>
+    </form>
+    <div class="card-body px-0 pb-2">
+        <table class="table table-bordered">
+            <thead class="table-success">
+                <tr>
+                    <th>ID</th>
+                    <th>Código Postal</th>
+                    <th>Asentamiento</th>
+                    <th>Tipo Asentamiento</th>
+                    <th>Delegacion/Municipio</th>
+                    <th>Estado</th>
+                    <th>Cuidad</th>
+                    <th>Código Administración Postal</th>
+                    <th>Clave Entidad</th>
+                    <th>Código Administración Postal</th>
+                    <th>Clave Tipo de Asentamiento</th>
+                    <th>Clave Municipio</th>
+                    <th>Identificador único del asentamiento</th>
+                    <th>Zona</th>
+                    <th>Clave Ciudad</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($codigosPostales as $codigoPostal)
+                    <tr>
+                        <td>{{ $codigoPostal->id }}</td>
+                        <td>{{ $codigoPostal->d_codigo }}</td>
+                        <td>{{ $codigoPostal->d_asenta }}</td>
+                        <td>{{ $codigoPostal->d_tipo_asenta }}</td>
+                        <td>{{ $codigoPostal->D_mnpio }}</td>
+                        <td>{{ $codigoPostal->d_estado }}</td>
+                        <td>{{ $codigoPostal->d_ciudad }}</td>
+                        <td>{{ $codigoPostal->d_CP }}</td>
+                        <td>{{ $codigoPostal->c_estado }}</td>
+                        <td>{{ $codigoPostal->c_oficina }}</td>
+                        <td>{{ $codigoPostal->c_tipo_asenta }}</td>
+                        <td>{{ $codigoPostal->c_mnpio }}</td>
+                        <td>{{ $codigoPostal->id_asenta_cpcons }}</td>
+                        <td>{{ $codigoPostal->d_zona }}</td>
+                        <td>{{ $codigoPostal->c_cve_ciudad }}</td>
+
+                    </tr>
+                @endforeach
         </table>
     </div>
-</div>
-<div class="card-footer d-flex justify-content-end">
-    <nav aria-label="Page navigation example">
-        <ul class="pagination">
-            {{-- Anterior --}}
-            @if($codigosPostales->currentPage() > 1)
-                <li class="page-item">
-                    <a class="page-link" href="{{ $codigosPostales->previousPageUrl() }}" aria-label="Previous">
-                        <span aria-hidden="true">&laquo;</span>
-                    </a>
-                </li>
-            @endif
+    </div>
+    <div class="card-footer d-flex justify-content-end">
+        <nav aria-label="Page navigation example">
+            <ul class="pagination">
+                {{-- Anterior --}}
+                @if ($codigosPostales->currentPage() > 1)
+                    <li class="page-item">
+                        <a class="page-link" href="{{ $codigosPostales->previousPageUrl() }}" aria-label="Previous">
+                            <span aria-hidden="true">&laquo;</span>
+                        </a>
+                    </li>
+                @endif
 
-            {{-- Páginas --}}
-            @for ($i = 1; $i <= $codigosPostales->lastPage(); $i++)
-                <li class="page-item {{ $codigosPostales->currentPage() == $i ? 'active' : '' }}">
-                    <a class="page-link" href="{{ $codigosPostales->url($i) }}">{{ $i }}</a>
-                </li>
-            @endfor
+                {{-- Páginas --}}
+                @for ($i = 1; $i <= $codigosPostales->lastPage(); $i++)
+                    <li class="page-item {{ $codigosPostales->currentPage() == $i ? 'active' : '' }}">
+                        <a class="page-link" href="{{ $codigosPostales->url($i) }}">{{ $i }}</a>
+                    </li>
+                @endfor
 
-            {{-- Siguiente --}}
-            @if($codigosPostales->currentPage() < $codigosPostales->lastPage())
-                <li class="page-item">
-                    <a class="page-link" href="{{ $codigosPostales->nextPageUrl() }}" aria-label="Next">
-                        <span aria-hidden="true">&raquo;</span>
-                    </a>
-                </li>
-            @endif
-        </ul>
-    </nav>
-</div>
+                {{-- Siguiente --}}
+                @if ($codigosPostales->currentPage() < $codigosPostales->lastPage())
+                    <li class="page-item">
+                        <a class="page-link" href="{{ $codigosPostales->nextPageUrl() }}" aria-label="Next">
+                            <span aria-hidden="true">&raquo;</span>
+                        </a>
+                    </li>
+                @endif
+            </ul>
+        </nav>
+    </div>
 
 @stop
 
@@ -112,7 +113,9 @@
 @stop
 
 @section('js')
-    <script> console.log('Hi!'); </script>
+    <script>
+        console.log('Hi!');
+    </script>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
 
@@ -124,7 +127,9 @@
                 $.ajax({
                     url: '/buscar-codigo-postal',
                     method: 'POST',
-                    data: { d_codigo: codigoPostal },
+                    data: {
+                        d_codigo: codigoPostal
+                    },
                     headers: {
                         'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
                     },
