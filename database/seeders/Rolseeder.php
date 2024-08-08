c69a5811504f2c0dcf932ca4f2327e3fafbd5ad1
Victor Valencia <valenciaalmeidavictor@gmail.com>
2024-08-07 22:55:50 -0600

    Parte de los evaluadores2 seeder

--- database/seeders/Rolseeder.php
+++ database/seeders/Rolseeder.php
@@ -13,142 +13,80 @@ class Rolseeder extends Seeder
      */
     public function run(): void
     {
-<<<<<<< HEAD
         // Crear roles
-        $role1 = Role::firstOrCreate(['name' => 'Admin']);
-        $role2 = Role::firstOrCreate(['name' => 'User']); // Asegúrate de crear el rol 'User'
-        $role3 = Role::firstOrCreate(['name' => 'Evaluador']);
-=======
-        $role1 = Role::create(['name' => 'Admin', 'guard_name' => 'web']);
-        $role2 = Role::create(['name' => 'Evaluador', 'guard_name' => 'web']);
-        $role3 = Role::create(['name' => 'User', 'guard_name' => 'web']);
->>>>>>> e0890295d6c83ee05dbe574b9004a51648d1f3ec
+        $roleAdmin = Role::firstOrCreate(['name' => 'Admin']);
+        $roleUser = Role::firstOrCreate(['name' => 'User']);
+        $roleEvaluator = Role::firstOrCreate(['name' => 'Evaluador']);
 
-        Permission::create(['name' => 'dashboard'])->syncRoles([$role1, $role2]);
+        // Crear permisos y asignarlos al rol Admin
+        $permissions = [
+            ['name' => 'dashboard', 'description' => 'Ver el Dashboard'],
+            ['name' => 'users.index', 'description' => 'Ver listado de Usuarios'],
+            ['name' => 'users.edit', 'description' => 'Asignar Un Rol'],
+            ['name' => 'users.update', 'description' => 'Actualizar Un Rol'],
+            ['name' => 'roles.index', 'description' => 'Ver listado de roles'],
+            ['name' => 'roles.show', 'description' => 'Vista de roles'],
+            ['name' => 'roles.create', 'description' => 'Crear un rol'],
+            ['name' => 'roles.edit', 'description' => 'Editar un rol'],
+            ['name' => 'roles.destroy', 'description' => 'Eliminar un rol'],
+            ['name' => 'permissions.index', 'description' => 'Tabla de permisos'],
+            ['name' => 'permissions.show', 'description' => 'Vista de permisos'],
+            ['name' => 'permissions.create', 'description' => 'Crear un permiso'],
+            ['name' => 'permissions.edit', 'description' => 'Editar un permiso'],
+            ['name' => 'permissions.destroy', 'description' => 'Eliminar un permiso'],
+        ];
 
-        Permission::create(['name' => 'codigos-postales'])->syncRoles([$role1, $role2]);
+        foreach ($permissions as $permissionData) {
+            $permission = Permission::firstOrCreate(
+                ['name' => $permissionData['name'], 'guard_name' => 'web'],
+                ['description' => $permissionData['description']]
+            );
 
-        Permission::create(['name' => 'users.index'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'users.edit'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'users.update'])->syncRoles([$role1, $role2]);
+            // Asignar el permiso al rol Admin
+            $permission->syncRoles([$roleAdmin]);
+        }
 
-        Permission::create(['name' => 'calendario.index']);
+        // Agregar permisos específicos para los roles User y Evaluador
+        $userPermissions = [
+            'users.index',
+            'users.edit',
+            'users.update',
+            'roles.index',
+            'roles.show',
+            'roles.create',
+            'roles.edit',
+            'roles.destroy',
+            'permissions.index',
+            'permissions.show',
+            'permissions.create',
+            'permissions.edit',
+            'permissions.destroy'
+        ];
 
-        Permission::create(['name' => 'competencias.agregar-fechas'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'competencias.guardar-fechas-modal'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'competencias.filtrarCompetencias'])->syncRoles([$role1, $role2]);
+        $evaluatorPermissions = [
+            'users.index',
+            'users.edit',
+            'users.update',
+            'roles.index',
+            'roles.show',
+            'roles.create',
+            'roles.edit',
+            'roles.destroy',
+            'permissions.index',
+            'permissions.show',
+            'permissions.create',
+            'permissions.edit',
+            'permissions.destroy'
+        ];
 
-        Permission::create(['name' => 'evaluadores.index'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'evaluadores.store'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'evaluadores.show'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'evaluadores.edit'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'evaluadores.update'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'evaluadores.destroy'])->syncRoles([$role1, $role2]);
+        foreach ($userPermissions as $perm) {
+            $permission = Permission::firstOrCreate(['name' => $perm, 'guard_name' => 'web']);
+            $permission->syncRoles([$roleUser]);
+        }
 
-        Permission::create(['name' => 'registroGeneral.index'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'registroGeneral.show'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'registroGeneral.updateDocumento'])->syncRoles([$role1, $role2]);
-
-        Permission::create(['name' => 'usuariosAdmin.index'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'usuariosAdmin.show'])->syncRoles([$role1, $role2]);
-
-        Permission::create(['name' => 'cursosExpediente.index'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'cursosExpediente.create'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'cursosExpediente.store'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'cursosExpediente.show'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'cursosExpediente.update'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'cursosExpediente.edit'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'cursosExpediente.destroy'])->syncRoles([$role1, $role2]);
-
-        Permission::create(['name' => 'evidenciasACU.index'])->syncRoles([$role1, $role2]);
-
-        Permission::create(['name' => 'competencia.index'])->syncRoles([$role1, $role2]);
-
-        Permission::create(['name' => 'evidenciasACO.index'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'evidenciasACO.updateEvidencia'])->syncRoles([$role1, $role2]);
-
-        Permission::create(['name' => 'fichas.show'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'ValidarFicha.updateDocumento'])->syncRoles([$role1, $role2]);
-
-        Permission::create(['name' => 'cartas.show'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'ValidarCarta.updateDocumento'])->syncRoles([$role1, $role2]);
-
-        Permission::create(['name' => 'documentosE.show'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'ValidarDocumento.updateDocumento'])->syncRoles([$role1, $role2]);
-
-        Permission::create(['name' => 'validarCoP.show'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'validarCoP.updateComprobante'])->syncRoles([$role1, $role2]);
-
-        Permission::create(['name' => 'validarCuP.show'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'validarCuP.updateComprobante'])->syncRoles([$role1, $role2]);
-
-        Permission::create(['name' => 'usuarios.index'])->assignRole($role3);
-
-        Permission::create(['name' => 'documentosUser.index'])->assignRole($role3);
-        Permission::create(['name' => 'documentosUser.store'])->assignRole($role3);
-        Permission::create(['name' => 'documentosUser.edit'])->assignRole($role3);
-        Permission::create(['name' => 'documentosUser.update'])->assignRole($role3);
-
-        Permission::create(['name' => 'competenciaEC.index'])->assignRole($role3);
-        Permission::create(['name' => 'competenciaEC.show'])->assignRole($role3);
-        Permission::create(['name' => 'competenciaEC.store'])->assignRole($role3);
-
-        Permission::create(['name' => 'registroCurso.index'])->assignRole($role3);
-        Permission::create(['name' => 'registroCurso.show'])->assignRole($role3);
-        Permission::create(['name' => 'registroCurso.store'])->assignRole($role3);
-
-        Permission::create(['name' => 'miscompetencias.index'])->assignRole($role3);
-        Permission::create(['name' => 'miscompetencias.resubir_comprobante'])->assignRole($role3);
-        Permission::create(['name' => 'miscompetencias.guardarResubirComprobante'])->assignRole($role3);
-
-        Permission::create(['name' => 'misCursos.index'])->assignRole($role3);
-        Permission::create(['name' => 'misCursos.resubir_comprobante'])->assignRole($role3);
-        Permission::create(['name' => 'misCursos.guardarResubirComprobante'])->assignRole($role3);
-
-        Permission::create(['name' => 'evidenciasEC.index'])->assignRole($role3);
-        Permission::create(['name' => 'evidenciasEC.show'])->assignRole($role3);
-        Permission::create(['name' => 'evidenciasEC.upload'])->assignRole($role3);
-        Permission::create(['name' => 'evidenciasEC.download'])->assignRole($role3);
-
-        Permission::create(['name' => 'Plan.show'])->assignRole($role3);
-        Permission::create(['name' => 'plan.store'])->assignRole($role3);
-
-        Permission::create(['name' => 'evidencias.resubir'])->assignRole($role3);
-        Permission::create(['name' => 'evidencias.resubir.submit'])->assignRole($role3);
-
-        Permission::create(['name' => 'mis.evidencias'])->assignRole($role3);
-
-        Permission::create(['name' => 'fechas.index'])->assignRole($role3);
-        Permission::create(['name' => 'fechas.show'])->assignRole($role3);
-        Permission::create(['name' => 'fechas.store'])->assignRole($role3);
-
-        Permission::create(['name' => 'evidenciasCU.index'])->assignRole($role3);
-        Permission::create(['name' => 'evidenciasCU.show'])->assignRole($role3);
-        Permission::create(['name' => 'evidenciasCU.upload'])->assignRole($role3);
-        Permission::create(['name' => 'evidenciasCU.download'])->assignRole($role3);
-
-        Permission::create(['name' => 'cursos.index'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'cursos.create'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'cursos.show'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'cursos.store'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'cursos.update'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'cursos.destroy'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'cursos.storeDocumento'])->syncRoles([$role1, $role2]);
-
-        Permission::create(['name' => 'roles.index'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'roles.create'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'roles.show'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'roles.store'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'roles.edit'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'roles.update'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'roles.destroy'])->syncRoles([$role1, $role2]);
-
-        Permission::create(['name' => 'permissions.index'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'permissions.create'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'permissions.show'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'permissions.store'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'permissions.edit'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'permissions.update'])->syncRoles([$role1, $role2]);
-        Permission::create(['name' => 'permissions.destroy'])->syncRoles([$role1, $role2]);
+        foreach ($evaluatorPermissions as $perm) {
+            $permission = Permission::firstOrCreate(['name' => $perm, 'guard_name' => 'web']);
+            $permission->syncRoles([$roleEvaluator]);
+        }
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -2,12 +2,10 @@
 
 namespace Database\Seeders;
 
-use App\Models\Administrador;
-use App\Models\Evaluadores;
 use App\Models\User;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
-use Spatie\Permission\Models\Role;
 
 class UserSeeder extends Seeder
 {
@@ -16,72 +14,56 @@ class UserSeeder extends Seeder
      */
     public function run(): void
     {
-        // Verifica que los roles existan antes de asignar
-        $adminRole = Role::firstOrCreate(['name' => 'Admin']);
-        $evaluadorRole = Role::firstOrCreate(['name' => 'Evaluador']);
-        $userRole = Role::firstOrCreate(['name' => 'User']);
-
-        Administrador::create([
-            'name' => 'Juan',
-            'secondName' => 'Gabriel',
-            'paternalSurname' => 'Contreras',
-            'maternalSurname' => 'Sansores',
+        // Crear usuario Admin
+        $admin = User::create([
+            'name' => 'Admin',
             'email' => 'admin@material.com',
-            'rol' => 'Admin',
-            'password' => Hash::make('12345'),
-            'email_verified_at' => now(), // Establece la fecha y hora actual como verificada
-        ])->assignRole($adminRole);
-
-        Evaluadores::create([
-            'name' => 'Fabiola',
-            'secondName' => 'Anel',
-            'paternalSurname' => 'Cuevas',
-            'maternalSurname' => 'López',
-            'rol' => 'Evaluador',
-            'email' => 'evaluador@material.com',
-            'password' => Hash::make('12345'),
-            'email_verified_at' => now(), // Establece la fecha y hora actual como verificada
-        ])->assignRole($evaluadorRole);
-
-<<<<<<< HEAD
-        $user1 = User::create([
-=======
-        User::create([
->>>>>>> e0890295d6c83ee05dbe574b9004a51648d1f3ec
-            'name' => 'Miguel',
-            'secondName' => 'Aleman',
-            'paternalSurname' => 'Rodriguez',
-            'maternalSurname' => 'Medina',
-            'email' => 'test@material.com',
-            'matricula' => '0001',
-            'password' => Hash::make('12345'),
-            'email_verified_at' => now(), // Establece la fecha y hora actual como verificada
-<<<<<<< HEAD
+            'password' => Hash::make('secret'),
+            'email_verified_at' => now(),
         ]);
 
-        $user1->assignRole('User');
+        $admin->assignRole('Admin');
 
-        $user2 = User::create([
-            'name' => 'Jose',
-            'secondName' => 'Gilberto',
-            'paternalSurname' => 'Martin',
-            'maternalSurname' => 'Perez',
-            'genero' => 'Hombre',
-            'phone' => '9956386893',
-            'matricula' => '0002',
-            'email' => 'test2@material.com',
-            'password' => Hash::make('12345'), // Asegúrate de cifrar la contraseña
-            'email_verified_at' => now(), // Establece la fecha y hora actual como verificada
-
+        // Crear usuario User
+        $user = User::create([
+            'name' => 'Angel',
+            'secondName' => 'Antonio',
+            'paternalSurname' => 'Canul',
+            'maternalSurname' => 'Chin',
+            'age' => '20',
+            'calle_avenida' => 'C18 entre 15 y 17',
+            'numext' => 'S/N',
+            'd_codigo' => '97818',
+            'd_asenta' => 'Kopomá',
+            'd_estado' => 'Yucatán',
+            'd_ciudad' => 'Kopomá',
+            'D_mnpio' => 'Kopomá',
+            'email' => 'test@example.com',
+            'password' => Hash::make('12345'),
+            'email_verified_at' => now(),
         ]);
 
-<<<<<<< HEAD
-        $user2->assignRole('User');
-=======
-        $admin->assignRole('User');
-=======
-        ])->assignRole($userRole);
->>>>>>> 340e9db59ab17f1155769fbccd747dc778a71be3
->>>>>>> e0890295d6c83ee05dbe574b9004a51648d1f3ec
+        $user->assignRole('User');
+
+        // Crear usuario Evaluador
+        $evaluator = User::create([
+            'name' => 'José',
+            'secondName' => 'Luis',
+            'paternalSurname' => 'Pérez',
+            'maternalSurname' => 'Moo',
+            'age' => '45',
+            'calle_avenida' => 'C32 entre 28 y 27',
+            'numext' => 'S/N',
+            'd_codigo' => '97800',
+            'd_asenta' => 'Maxcanú',
+            'd_estado' => 'Yucatán',
+            'd_ciudad' => 'Maxcanú',
+            'D_mnpio' => 'Maxcanú',
+            'email' => 'test2@example.com',
+            'password' => Hash::make('12345'),
+            'email_verified_at' => now(),
+        ]);
+
+        $evaluator->assignRole('Evaluador');
     }
 }
